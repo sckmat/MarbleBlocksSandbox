fffd2042b96ddb0738650d39fc33c039eac1627d
Vladislav Yegorov <schmat03@gmail.com>
2023-10-07 18:36:24 +0500

    Log Reg

---
+++ Assets/Scripts/Auth/UISwitcher.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISwitcher : MonoBehaviour
+{
+    [SerializeField] private GameObject register;
+    [SerializeField] private GameObject login;
+
+    public static UISwitcher Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != null)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }    
+    }
+
+    public void LoginOn()
+    {
+        register.SetActive(false);
+        login.SetActive(true);
+    }
+    
+    public void RegisterOn()
+    {
+        login.SetActive(false);
+        register.SetActive(true);
+    }
+}
